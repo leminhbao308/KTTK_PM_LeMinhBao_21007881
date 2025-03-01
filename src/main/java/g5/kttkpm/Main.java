efc8587de428a312ae9f3fdd285827942d353e4f
Lê Minh Bảo <besilent308@gmail.com>
2025-03-01 07:55:46 +0700

    feat(StaticBlockSingleton): Create example using StaticBlockSingleton

--- src/main/java/g5/kttkpm/Main.java
+++ src/main/java/g5/kttkpm/Main.java
@@ -1,16 +1,24 @@
 package g5.kttkpm;
 
 import g5.kttkpm.singleton.EagerInitializedSingleton;
+import g5.kttkpm.singleton.StaticBlockSingleton;
 
 public class Main {
     
     public static void main(String[] args) {
         // EagerInitializedSingleton
-        EagerInitializedSingleton eis1 = EagerInitializedSingleton.getInstance();
-        EagerInitializedSingleton eis2 = EagerInitializedSingleton.getInstance();
+        // EagerInitializedSingleton eis1 = EagerInitializedSingleton.getInstance();
+        // EagerInitializedSingleton eis2 = EagerInitializedSingleton.getInstance();
         
-        System.out.println("EagerInitializedSingleton 1: " + eis1.hashCode());
-        System.out.println("EagerInitializedSingleton 2: " + eis2.hashCode());
+        // System.out.println("EagerInitializedSingleton 1: " + eis1.hashCode());
+        // System.out.println("EagerInitializedSingleton 2: " + eis2.hashCode());
+        
+        // StaticBlockSingleton
+        StaticBlockSingleton sbs1 = StaticBlockSingleton.getInstance();
+        StaticBlockSingleton sbs2 = StaticBlockSingleton.getInstance();
+        
+        System.out.println("StaticBlockSingleton 1: " + sbs1.hashCode());
+        System.out.println("StaticBlockSingleton 2: " + sbs2.hashCode());
         
     }
 }
---
+++ src/main/java/g5/kttkpm/singleton/StaticBlockSingleton.java
@@ -0,0 +1,21 @@
+package g5.kttkpm.singleton;
+
+public class StaticBlockSingleton {
+    private static final StaticBlockSingleton instance;
+    
+    private StaticBlockSingleton() {
+    }
+    
+    //static block initialization for exception handling
+    static {
+        try {
+            instance = new StaticBlockSingleton();
+        } catch (Exception e) {
+            throw new RuntimeException("Exception occured in creating singleton instance");
+        }
+    }
+    
+    public static StaticBlockSingleton getInstance() {
+        return instance;
+    }
+}
