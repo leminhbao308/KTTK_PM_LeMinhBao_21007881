3eafcff64b91ab6dd2268dd323d5e2713e4274ff
Lê Minh Bảo <besilent308@gmail.com>
2025-03-01 08:32:56 +0700

    refactor(Main): Change all example to each method

--- src/main/java/g5/kttkpm/Main.java
+++ src/main/java/g5/kttkpm/Main.java
@@ -1,56 +1,90 @@
 package g5.kttkpm;
 
-import g5.kttkpm.singleton.SerializedSingleton;
+import g5.kttkpm.singleton.*;
 
 import java.io.*;
+import java.lang.reflect.Constructor;
 
 public class Main {
     
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         // EagerInitializedSingleton
-        // EagerInitializedSingleton eis1 = EagerInitializedSingleton.getInstance();
-        // EagerInitializedSingleton eis2 = EagerInitializedSingleton.getInstance();
-        
-        // System.out.println("EagerInitializedSingleton 1: " + eis1.hashCode());
-        // System.out.println("EagerInitializedSingleton 2: " + eis2.hashCode());
+        eagerInitializedSingleton();
         
         // StaticBlockSingleton
-        // StaticBlockSingleton sbs1 = StaticBlockSingleton.getInstance();
-        // StaticBlockSingleton sbs2 = StaticBlockSingleton.getInstance();
-        
-        // System.out.println("StaticBlockSingleton 1: " + sbs1.hashCode());
-        // System.out.println("StaticBlockSingleton 2: " + sbs2.hashCode());
+        staticBlockSingleton();
         
         // LazyInitializedSingleton
-        // LazyInitializedSingleton lis1 = LazyInitializedSingleton.getInstance();
-        // LazyInitializedSingleton lis2 = LazyInitializedSingleton.getInstance();
-        
-        // System.out.println("LazyInitializedSingleton 1: " + lis1.hashCode());
-        // System.out.println("LazyInitializedSingleton 2: " + lis2.hashCode());
+        lazyInitializedSingleton();
         
         // ThreadSafeSingleton
-        // ThreadSafeSingleton tss1 = ThreadSafeSingleton.getInstance();
-        // ThreadSafeSingleton tss2 = ThreadSafeSingleton.getInstance();
-        
-        // System.out.println("ThreadSafeSingleton 1: " + tss1.hashCode());
-        // System.out.println("ThreadSafeSingleton 2: " + tss2.hashCode());
+        threadSafeSingleton();
         
         // ThreadSafeSingleton Double Check Locking
-        // ThreadSafeSingleton dcl1 = ThreadSafeSingleton.getInstanceUsingDoubleLocking();
-        // ThreadSafeSingleton dcl2 = ThreadSafeSingleton.getInstanceUsingDoubleLocking();
-        
-        // System.out.println("ThreadSafeSingleton Double Check Locking 1: " + dcl1.hashCode());
-        // System.out.println("ThreadSafeSingleton Double Check Locking 2: " + dcl2.hashCode());
+        threadSafeSingletonDoubleCheckLocking();
         
         // BillPughSingleton
-        // BillPughSingleton bps1 = BillPughSingleton.getInstance();
-        // BillPughSingleton bps2 = BillPughSingleton.getInstance();
-        
-        // System.out.println("BillPughSingleton 1: " + bps1.hashCode());
-        // System.out.println("BillPughSingleton 2: " + bps2.hashCode());
+        billPughSingleton();
         
         // ReflectionSingletonTest
-        /*
+        reflectionSingleton();
+        
+        // EnumSingleton
+        enumSingleton();
+        
+        // SerializedSingleton
+        serializedSingleton();
+    }
+    
+    static void eagerInitializedSingleton() {
+        EagerInitializedSingleton eis1 = EagerInitializedSingleton.getInstance();
+        EagerInitializedSingleton eis2 = EagerInitializedSingleton.getInstance();
+        
+        System.out.println("EagerInitializedSingleton 1: " + eis1.hashCode());
+        System.out.println("EagerInitializedSingleton 2: " + eis2.hashCode());
+    }
+    
+    static void staticBlockSingleton() {
+        StaticBlockSingleton sbs1 = StaticBlockSingleton.getInstance();
+        StaticBlockSingleton sbs2 = StaticBlockSingleton.getInstance();
+        
+        System.out.println("StaticBlockSingleton 1: " + sbs1.hashCode());
+        System.out.println("StaticBlockSingleton 2: " + sbs2.hashCode());
+    }
+    
+    static void lazyInitializedSingleton() {
+        LazyInitializedSingleton lis1 = LazyInitializedSingleton.getInstance();
+        LazyInitializedSingleton lis2 = LazyInitializedSingleton.getInstance();
+        
+        System.out.println("LazyInitializedSingleton 1: " + lis1.hashCode());
+        System.out.println("LazyInitializedSingleton 2: " + lis2.hashCode());
+    }
+    
+    static void threadSafeSingleton() {
+        ThreadSafeSingleton tss1 = ThreadSafeSingleton.getInstance();
+        ThreadSafeSingleton tss2 = ThreadSafeSingleton.getInstance();
+        
+        System.out.println("ThreadSafeSingleton 1: " + tss1.hashCode());
+        System.out.println("ThreadSafeSingleton 2: " + tss2.hashCode());
+    }
+    
+    static void threadSafeSingletonDoubleCheckLocking() {
+        ThreadSafeSingleton dcl1 = ThreadSafeSingleton.getInstanceUsingDoubleLocking();
+        ThreadSafeSingleton dcl2 = ThreadSafeSingleton.getInstanceUsingDoubleLocking();
+        
+        System.out.println("ThreadSafeSingleton Double Check Locking 1: " + dcl1.hashCode());
+        System.out.println("ThreadSafeSingleton Double Check Locking 2: " + dcl2.hashCode());
+    }
+    
+    static void billPughSingleton() {
+        BillPughSingleton bps1 = BillPughSingleton.getInstance();
+        BillPughSingleton bps2 = BillPughSingleton.getInstance();
+        
+        System.out.println("BillPughSingleton 1: " + bps1.hashCode());
+        System.out.println("BillPughSingleton 2: " + bps2.hashCode());
+    }
+    
+    static void reflectionSingleton() {
         EagerInitializedSingleton instanceOne =
             EagerInitializedSingleton.getInstance();
         EagerInitializedSingleton instanceTwo = null;
@@ -69,17 +103,14 @@ public class Main {
         }
         System.out.println("Instance 1: " + instanceOne.hashCode());
         System.out.println("Instance 2: " + instanceTwo.hashCode());
-         */
+    }
+    
+    static void enumSingleton() {
+        EnumSingleton es1 = EnumSingleton.INSTANCE;
+        EnumSingleton es2 = EnumSingleton.INSTANCE;
         
-        // EnumSingleton
-        // EnumSingleton es1 = EnumSingleton.INSTANCE;
-        // EnumSingleton es2 = EnumSingleton.INSTANCE;
-        
-        // System.out.println("EnumSingleton 1: " + es1.hashCode());
-        // System.out.println("EnumSingleton 2: " + es2.hashCode());
-        
-        // SerializedSingleton
-        serializedSingleton();
+        System.out.println("EnumSingleton 1: " + es1.hashCode());
+        System.out.println("EnumSingleton 2: " + es2.hashCode());
     }
     
     static void serializedSingleton() throws IOException, ClassNotFoundException {
@@ -94,7 +125,7 @@ public class Main {
         SerializedSingleton ss2 = (SerializedSingleton) in.readObject();
         in.close();
         
-        System.out.println("SerializedSingleton 1: "+ss1.hashCode());
-        System.out.println("SerializedSingleton 2: "+ss2.hashCode());
+        System.out.println("SerializedSingleton 1: " + ss1.hashCode());
+        System.out.println("SerializedSingleton 2: " + ss2.hashCode());
     }
 }
