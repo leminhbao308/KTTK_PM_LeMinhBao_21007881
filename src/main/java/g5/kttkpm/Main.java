b2e6fc1c1d38b4c8fb0802a3507312faee0e581c
Lê Minh Bảo <besilent308@gmail.com>
2025-03-01 07:54:44 +0700

    feat(EagerInitializeSingleton): Create example using EagerInitializedSingleton

--- src/main/java/g5/kttkpm/Main.java
+++ src/main/java/g5/kttkpm/Main.java
@@ -1,7 +1,16 @@
 package g5.kttkpm;
 
+import g5.kttkpm.singleton.EagerInitializedSingleton;
+
 public class Main {
+    
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // EagerInitializedSingleton
+        EagerInitializedSingleton eis1 = EagerInitializedSingleton.getInstance();
+        EagerInitializedSingleton eis2 = EagerInitializedSingleton.getInstance();
+        
+        System.out.println("EagerInitializedSingleton 1: " + eis1.hashCode());
+        System.out.println("EagerInitializedSingleton 2: " + eis2.hashCode());
+        
     }
 }
